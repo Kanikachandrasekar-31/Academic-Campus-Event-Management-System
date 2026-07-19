3cf3dbc7a37fb13264c25f4d5175c0713a6fa241
Kanikachandrasekar-31 <kanikachandrasekar@gmail.com>
2026-07-19 22:04:56 +0530

    Delete FacultyRepository.java

--- FacultyRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.Campushub.repository;
-
-import com.example.Campushub.entity.Faculty;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FacultyRepository extends JpaRepository<Faculty, Long> {
-}
