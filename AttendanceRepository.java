15213f54a3267e68223696c051e9b7c6c30122da
Kanikachandrasekar-31 <kanikachandrasekar@gmail.com>
2026-07-19 16:42:30 +0530

    Delete AttendanceRepository.java

--- AttendanceRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.Campushub.repository;
-
-import com.example.Campushub.entity.Attendance;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AttendanceRepository extends JpaRepository<Attendance, Long> {
-}
