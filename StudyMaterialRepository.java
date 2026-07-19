fd2649c42851c92ada7e0a972e2e7450e611387b
Kanikachandrasekar-31 <kanikachandrasekar@gmail.com>
2026-07-19 16:51:39 +0530

    Delete StudyMaterialRepository.java

--- StudyMaterialRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.Campushub.repository;
-
-import com.example.Campushub.entity.StudyMaterial;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudyMaterialRepository extends JpaRepository<StudyMaterial, Long> {
-}
