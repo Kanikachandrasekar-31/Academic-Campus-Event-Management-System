7bbfebab514e84c346f1432dfa25b152e3bb115e
Kanikachandrasekar-31 <kanikachandrasekar@gmail.com>
2026-07-19 16:54:10 +0530

    Delete EventRepository.java

--- EventRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.Campushub.repository;
-
-import com.example.Campushub.entity.Event;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EventRepository extends JpaRepository<Event, Long> {
-}
