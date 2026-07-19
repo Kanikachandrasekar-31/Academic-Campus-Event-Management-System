abe1b894fe5adafd3ea40f12c338517b3937a04f
Kanikachandrasekar-31 <kanikachandrasekar@gmail.com>
2026-07-19 16:47:31 +0530

    Delete VenueRepository.java

--- VenueRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.Campushub.repository;
-
-import com.example.Campushub.entity.Venue;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VenueRepository extends JpaRepository<Venue, Long> {
-}
