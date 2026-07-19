519461c4e9fa144250106ad7831dfcc56966593f
Kanikachandrasekar-31 <kanikachandrasekar@gmail.com>
2026-07-19 16:45:31 +0530

    Delete ClassGroupRepository.java

--- ClassGroupRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.Campushub.repository;
-
-import com.example.Campushub.entity.ClassGroup;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClassGroupRepository extends JpaRepository<ClassGroup, Long> {
-}
