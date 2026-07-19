1e44fef56c3db5f5d40582634a9f472b806dd224
Kanikachandrasekar-31 <kanikachandrasekar@gmail.com>
2026-07-19 16:41:29 +0530

    Delete AssignmentRepository.java

--- AssignmentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.Campushub.repository;
-
-import com.example.Campushub.entity.Assignment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AssignmentRepository extends JpaRepository<Assignment, Long> {
-}
